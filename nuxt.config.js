e0d16e15b217f2099957c001e47b071c14a75509
the6thm0nth <the6thm0nth@outlook.com>
2018-07-01 13:47:33 +0900

    Set: main config - use @nuxt/axios intead of original axios on pages to avoid bugs - change ahn heejong's info to mine

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,90 +1,91 @@
-const axios = require('axios')
-const S3_BASE_PATH = 'https://s3.ap-northeast-2.amazonaws.com/ahnheejong.name-articles'
-
-const title = 'ahn [at] 🇰🇷'
-const url = 'http://ahnheejong.name'
+const axios = require("axios");
+const title = "the6thm0nth.net";
+const url = "https://the6thm0nth.net";
 
 const meta = {
-  'og:locale': 'ko_kR',
-  'og:site_name': 'ahj [at] 🇰🇷',
-  'og:title': title,
-  'og:image': 'http://ahnheejong.name/logo.png',
-  'og:image:type': 'image/png',
-  'og:image:width': '1200',
-  'og:image:height': '630',
-  'og:url': url,
-  'twitter:site': '@heejongahn',
-  'twitter:title': title,
-  'twitter:description': '👋',
-  'twitter:url': url,
-  'twitter:card': 'summary_large_image',
-  'twitter:image': 'http://ahnheejong.name/logo.png',
-  'twitter:creator': 'ahn heejong'
-}
+  "og:locale": "ko_kR",
+  "og:site_name": "Hoony Chang",
+  "og:title": title,
+  "og:image": `${url}/the6thm0nth.jpg`,
+  "og:image:type": "image/jpg",
+  "og:image:width": "1200",
+  "og:image:height": "630",
+  "og:url": url,
+  "twitter:site": "@the6thm0nth",
+  "twitter:title": title,
+  "twitter:description": "Mangwon-dong Basketballer & FTTO Coordinator",
+  "twitter:url": url,
+  "twitter:card": "summary_large_image",
+  "twitter:image": `${url}/logo.png`,
+  "twitter:creator": "Hoony Chang"
+};
 
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'about [dot] ahj [at] 🇰🇷',
+    title: "the6thm0nth.net",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '👋' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Mangwon-dong Basketballer & FTTO Coordinator"
+      },
       ...Object.keys(meta).map(name => ({
         hid: name,
         name,
         content: meta[name]
       }))
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/i.png' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/the6thm0nth.jpg" }]
   },
-  css: [
-    { src: '~assets/style.scss', lang: 'scss' }
-  ],
+  css: [{ src: "~assets/style.scss", lang: "scss" }],
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
   generate: {
-    routes: function () {
-      return axios(`${S3_BASE_PATH}/index.json`, { responseType: 'json' })
-      .then(r => {
+    routes: function() {
+      return axios(`/blog/index.json`, { responseType: "json" }).then(r => {
         return r.data.map(article => {
           const route = {
             route: `/articles/${article.slug}`,
             payload: article
-          }
-          return route
-        })
-      })
+          };
+          return route;
+        });
+      });
     }
   },
   build: {
     /*
     ** Run ESLINT on save
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   },
   modules: [
-    ['@nuxtjs/google-analytics', {
-      ua: 'UA-102906433-1',
-      id: 'UA-102906433-1'
-    }]
+    [
+      "@nuxtjs/axios",
+      "@nuxtjs/google-analytics",
+      {
+        ua: "UA-121696619-1",
+        id: "UA-102906433-1"
+      }
+    ]
   ]
-}
+};
