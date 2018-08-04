21148a9c1e4baba6651ff8ae2f5aae34dde6b14e
the6thm0nth <the6thm0nth@outlook.com>
2018-08-04 16:26:57 +0900

    Remove: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: "standard",
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ["html"],
   // add your custom rules here
   rules: {},
   globals: {}
